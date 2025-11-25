c4d8c1191a624fbb389080618d8d271aeb1aa1c4
xxzeroeight <xxzeroeight@naver.com>
2025-11-25 16:18:23 +0900

    sprint-mission-2(Repository) Test

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -4,9 +4,21 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.enums.ChannelType;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
@@ -118,6 +130,19 @@ public class JavaApplication
 
     }
 
+    static User setUpUser(UserService userService) {
+        return userService.create("woody", "woody1234", "woody@codeit.com");
+    }
+
+    static Channel setUpChannel(ChannelService channelService) {
+        return channelService.create("공지", "공지 채널입니다.", ChannelType.PUBLIC);
+    }
+
+    static void messageCreateTest(MessageService messageService, User user, Channel channel) {
+        Message m =  messageService.create("안녕하세요", user.getId(), channel.getId());
+        System.out.println("메시지 생성: " + m.getId());
+    }
+
     public static void main(String[] args) {
         System.out.println("\n-----------------------In-Memory Test-----------------------");
 
@@ -141,5 +166,36 @@ public class JavaApplication
         /* JCF*Service VS File*Service */
         // 공: "비즈니스 로직"과 "저장 로직"이 함께 존재.
         // 차: JCF*Service는 In-Memory(휘발), File*Service는 File에 저장(비휘발)
+
+        System.out.println("\n-----------------------JCF Repository Test-----------------------");
+        UserRepository jcfUserRepository = JCFUserRepository.getInstance();
+        ChannelRepository jcfChannelRepository = JCFChannelRepository.getInstance();
+        MessageRepository jcfMessageRepository = JCFMessageRepository.getInstance();
+
+        UserService jcfRepositoryUserService = new BasicUserService(jcfUserRepository);
+        ChannelService jcfRepositoryChannelService = new BasicChannelService(jcfChannelRepository);
+        MessageService jcfRepositoryMessageService = new BasicMessageService(jcfMessageRepository);
+
+        User jcfUser = setUpUser(jcfRepositoryUserService);
+        Channel jcfChannel = setUpChannel(jcfRepositoryChannelService);
+
+        messageCreateTest(jcfRepositoryMessageService, jcfUser, jcfChannel);
+
+        System.out.println("\n-----------------------File Repository Test-----------------------");
+        UserRepository fileUserRepository = FileUserRepository.getInstance();
+        ChannelRepository fileChannelRepository = FileChannelRepository.getInstance();
+        MessageRepository fileMessageRepository = FileMessageRepository.getInstance();
+
+        UserService fileRepositoryUserService = new BasicUserService(fileUserRepository);
+        ChannelService fileRepositoryChannelService = new BasicChannelService(fileChannelRepository);
+        MessageService fileRepositoryMessageService = new BasicMessageService(fileMessageRepository);
+
+        User fileUser = setUpUser(fileRepositoryUserService);
+        Channel fileChannel = setUpChannel(fileRepositoryChannelService);
+
+        messageCreateTest(fileRepositoryMessageService, fileUser, fileChannel);
     }
+
+    /* JCF*Service or File*Service와의 차이점 */
+    // "비즈니스 로직"과 "저장 로직"이 확실하게 분리되어 코드 작성이나 유지보수에 좋음.
 }
