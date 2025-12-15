91b330c64d8818346838af28840bfc7e65b6c6a5
xxzeroeight <xxzeroeight@naver.com>
2025-12-15 13:10:50 +0900

    feat: 새로운 도메인 추가
    - BinaryContent, ReadStatus, UserStatus 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class BinaryContent implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    private final UUID id;
+
+    private String fileName;
+    private Long size;
+    private String contentType;
+    private byte[] bytes;
+
+    private Instant createdAt;
+
+    public BinaryContent(String fileName, Long size, String contentType, byte[] bytes, Instant createdAt) {
+        this.id = UUID.randomUUID();
+        this.fileName = fileName;
+        this.size = size;
+        this.contentType = contentType;
+        this.bytes = bytes;
+        this.createdAt = Instant.now();
+    }
+}
