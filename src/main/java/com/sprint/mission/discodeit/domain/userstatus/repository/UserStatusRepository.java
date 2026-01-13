acb6ece9ac4fc98f83b3ffffb91656cfdc979e8c
xxzeroeight <xxzeroeight@naver.com>
2026-01-13 18:40:28 +0900

    refactor: UserStatus Service, Repository에 JPA 적용

--- src/main/java/com/sprint/mission/discodeit/domain/userstatus/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/domain/userstatus/repository/UserStatusRepository.java
@@ -1,18 +1,16 @@
 package com.sprint.mission.discodeit.domain.userstatus.repository;
 
 import com.sprint.mission.discodeit.domain.userstatus.domain.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserStatusRepository
+public interface UserStatusRepository extends JpaRepository<UserStatus, UUID>
 {
-    UserStatus save(UserStatus userStatus);
-    Optional<UserStatus> findById(UUID id);
     Optional<UserStatus> findByUserId(UUID userId);
-    List<UserStatus> findAll();
+
     boolean existsById(UUID id);
-    void deleteById(UUID id);
+
     void deleteByUserId(UUID userId);
 }
--- src/main/java/com/sprint/mission/discodeit/domain/userstatus/service/BasicUserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/domain/userstatus/service/BasicUserStatusService.java
@@ -1,18 +1,15 @@
 package com.sprint.mission.discodeit.domain.userstatus.service;
 
-import com.sprint.mission.discodeit.domain.userstatus.dto.domain.UserStatusDto;
-import com.sprint.mission.discodeit.domain.userstatus.dto.request.UserStatusCreateRequest;
-import com.sprint.mission.discodeit.domain.userstatus.dto.request.UserStatusUpdateRequest;
-import com.sprint.mission.discodeit.domain.userstatus.domain.UserStatus;
-import com.sprint.mission.discodeit.domain.user.exception.DuplicateUserException;
-import com.sprint.mission.discodeit.domain.user.exception.UserNotFoundException;
-import com.sprint.mission.discodeit.domain.userstatus.exception.UserStatusNotFoundException;
 import com.sprint.mission.discodeit.domain.user.repository.UserRepository;
+import com.sprint.mission.discodeit.domain.userstatus.domain.UserStatus;
+import com.sprint.mission.discodeit.domain.userstatus.dto.domain.UserStatusDto;
+import com.sprint.mission.discodeit.domain.userstatus.dto.request.UserStatusUpdateRequest;
+import com.sprint.mission.discodeit.domain.userstatus.exception.UserStatusNotFoundException;
 import com.sprint.mission.discodeit.domain.userstatus.repository.UserStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,64 +19,15 @@ public class BasicUserStatusService implements UserStatusService
     private final UserStatusRepository userStatusRepository;
     private final UserRepository userRepository;
 
-    @Override
-    public UserStatusDto create(UserStatusCreateRequest userStatusCreateRequest) {
-        if (!userRepository.existsById(userStatusCreateRequest.userId())) {
-            throw UserNotFoundException.byId(userStatusCreateRequest.userId());
-        }
-
-        if (userStatusRepository.findByUserId(userStatusCreateRequest.userId()).isPresent()) {
-            throw DuplicateUserException.byId(userStatusCreateRequest.userId());
-        }
-
-        UserStatus userStatus = new UserStatus(userStatusCreateRequest.userId(), userStatusCreateRequest.lastActiveAt());
-        UserStatus savedUserStatus = userStatusRepository.save(userStatus);
-
-        return UserStatusDto.from(savedUserStatus);
-    }
-
-    @Override
-    public UserStatusDto find(UUID userStatusId) {
-        return userStatusRepository.findById(userStatusId)
-                .map(UserStatusDto::from)
-                .orElseThrow(() -> UserStatusNotFoundException.byId(userStatusId));
-    }
-
-    @Override
-    public List<UserStatusDto> findAll() {
-        return userStatusRepository.findAll().stream()
-                .map(UserStatusDto::from)
-                .toList();
-    }
-
-    @Override
-    public UserStatusDto update(UUID userStatusId, UserStatusUpdateRequest userStatusUpdateRequest) {
-        UserStatus userStatus = userStatusRepository.findById(userStatusId)
-                .orElseThrow(() -> UserStatusNotFoundException.byId(userStatusId));
-
-        userStatus.update(userStatusUpdateRequest.newLastActiveAt());
-        UserStatus savedUserStatus = userStatusRepository.save(userStatus);
-
-        return UserStatusDto.from(savedUserStatus);
-    }
-
+    // UserRepository.findById()로 User -> UserStatus로 접근할 수 있으므로 사실상 UserService로 통합 가능함.
+    @Transactional
     @Override
     public UserStatusDto updateByUserId(UUID userId, UserStatusUpdateRequest userStatusUpdateRequest) {
         UserStatus userStatus = userStatusRepository.findByUserId(userId)
                 .orElseThrow(() -> UserStatusNotFoundException.byId(userId));
 
         userStatus.update(userStatusUpdateRequest.newLastActiveAt());
-        UserStatus savedUserStatus = userStatusRepository.save(userStatus);
 
-        return UserStatusDto.from(savedUserStatus);
-    }
-
-    @Override
-    public void delete(UUID userStatusId) {
-        if (!userStatusRepository.existsById(userStatusId)) {
-            throw UserStatusNotFoundException.byId(userStatusId);
-        }
-
-        userStatusRepository.deleteById(userStatusId);
+        return UserStatusDto.from(userStatus);
     }
 }
