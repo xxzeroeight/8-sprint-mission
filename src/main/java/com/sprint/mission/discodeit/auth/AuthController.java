9c076153ecc2ffcb40e76442f231f342ba0a3503
xxzeroeight <xxzeroeight@naver.com>
2026-03-25 21:08:32 +0900

    feat: 인증 - 세션을 활용한 현재 사용자 정보 조회

--- src/main/java/com/sprint/mission/discodeit/auth/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/auth/AuthController.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.auth;
 
+import com.sprint.mission.discodeit.auth.service.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.domain.user.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,14 @@ public class AuthController
         String tokenValue = csrfToken.getToken();
         log.debug("CSRF 토큰 요청: {}", tokenValue);
 
-        return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getCurrentUser(@AuthenticationPrincipal DiscodeitUserDetails discodeitUserDetails)
+    {
+        log.debug("AuthController 세션 기반 사용자 정보 조회 요청: {}", discodeitUserDetails.getUsername());
+
+        return ResponseEntity.ok(discodeitUserDetails.getUserResponse());
     }
 }
